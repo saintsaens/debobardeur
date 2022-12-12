12abfcdf1e1a464670f2270a16897ef0f4b88fd9
Flavien <saintsaens@users.noreply.github.com>
2022-12-12 16:34:24 +0100

    Capitalize text after a period.

--- capitalization.py
+++ capitalization.py
@@ -6,10 +6,20 @@ def fix_capitalization_at_start_of_text(text):
     return text
 
 
+def fix_capitalization_after_period(text):
+    for i in text:
+        if i.find(".") != -1:
+            period_index = text.index(i)
+            text[period_index + 1] = text[period_index + 1].capitalize()
+
+    return text
+
+
 def fix_capitalization(text):
     # Create a list of the text, including punctuation.
     split_text = re.split("(\W+)", text)
     split_text = fix_capitalization_at_start_of_text(split_text)
+    split_text = fix_capitalization_after_period(split_text)
 
     # Stitch the list back together.
     reunited_text = ""
