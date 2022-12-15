59e9ff40d626ae41cf6722fcc65118bf67a0b6fe
Flavien <saintsaens@users.noreply.github.com>
2022-12-15 10:28:06 +0100

    Add logic when clicking button
    - Renamed base.html into index.html
    - Clicking button "Débobardiser" prints static text below.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,16 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, flash, redirect, url_for
 
 
 app = Flask(__name__)
+app.secret_key = "38a835526880f08e47b457a5e87d768f6a5ac32a1f2ab408d6fd0ef3ac10c181"
 
 
 @app.route("/")
-def debobardeur_form():
-    return render_template("base.html")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/debobarded")
+def debobarded():
+    flash('Texte débobardé. 🎉')
+    return redirect(url_for('index'))
