ba40e045bea136c57e1f2a0324ce470a4636a423
Flavien <saintsaens@users.noreply.github.com>
2022-12-16 09:43:31 +0100

    Remove useless punctuation and capitalization handling.
    Both operations are now handled in a separate file.

--- text_manipulation.py
+++ text_manipulation.py
@@ -10,28 +10,9 @@ def remove_element_from_text(text, element):
     element_index = split_text.index(element)
     split_text.pop(element_index)
 
-    # Adapt punctuation and capitalization.
-    adapt_capitalization(split_text, element_index)
-
     # Stitch the list back together.
     reunited_text = ""
     for x in range(0, len(split_text)):
         reunited_text = reunited_text + split_text[x]
 
     return reunited_text
-
-
-def adapt_punctuation(text, index):
-    # If the removed element was at the start of the text, remove the now leading punctuation.
-    if index == 0:
-        text.pop(index)
-
-    # If there is a space before, remove it.
-    else:
-        text.pop(index - 1)
-
-
-def adapt_capitalization(text, index):
-    # If it’s at the start of the text, capitalize.
-    if index == 0:
-        text[index] = text[index].capitalize()
