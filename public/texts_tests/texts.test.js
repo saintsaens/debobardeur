372e6b8e4357f4329799110ea8b1a38af246fd4b
Flavien <saintsaens@users.noreply.github.com>
2023-01-26 10:35:51 +0100

    Run tests on all test texts exported from Notion.

--- public/texts_tests/texts.test.js
+++ public/texts_tests/texts.test.js
@@ -1,12 +1,24 @@
 const fs = require('fs');
+
 const path = require('path');
 const debobardize = require(path.join(__dirname, "../javascripts/debobardeur"));
 
-test('debobardizes txt files', () => {
-  const filePath = path.join(__dirname, "./support-answer.txt");
-  const data = fs.readFileSync(filePath, 'utf8');
-  const test = data.split("-----").map((element) => element.trim());
-  const input = test[0];
-  const output = test[1];
-  expect(debobardize.debobardize(input)).toStrictEqual(output);
+test('debobardizes txt files', async () => {
+  let txtFiles = [];
+  const files = await fs.promises.readdir(__dirname);
+  for (let file of files) {
+    if (file.endsWith('.txt')) {
+      txtFiles.push(file);
+    }
+  }
+
+  for (let file of txtFiles) {
+    const filePath = path.join(__dirname, "./" + file);
+    const data = fs.readFileSync(filePath, 'utf8');
+    const test = data.split("-----").map((element) => element.trim());
+    const input = test[0];
+    const output = test[1];
+
+    expect(debobardize.debobardize(input)).toBe(output);
+  }
 });
