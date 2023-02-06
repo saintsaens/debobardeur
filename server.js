f38cc0d4b6e19a709a0f86b53b05b2bb2f50ed8b
Flavien <saintsaens@users.noreply.github.com>
2023-02-06 19:59:46 +0100

    Turn app.js + index.js into lightweight server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { debobardize } from './src/debobardeur.js';
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  const message = req.body.message
+
+  const newText = debobardize(message)
+
+  res.send(newText);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
