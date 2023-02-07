1e3d03602eaf1abdea22963e7f36f7e0c3fd19d0
Flavien <saintsaens@users.noreply.github.com>
2023-02-07 12:50:08 +0100

    Move all imports at the top.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import cors from 'cors'
-
+import * as dotenv from 'dotenv'
 import { debobardize } from './src/debobardeur.js';
+
 const app = express()
 const port = 3000
 
 app.use(cors());
 app.use(express.json());
 
-import * as dotenv from 'dotenv'
 dotenv.config()
 
 app.get("/", (req, res) => {
