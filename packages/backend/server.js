1a8b3ca310af9b2985568190a784eeb60c071c72
Flavien <saintsaens@users.noreply.github.com>
2023-02-13 11:48:28 +0100

    Save submitted text to supabase.

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 import { debobardize } from './src/debobardeur.js';
+import { connectToSupabase, addEntry } from './src/supabase.js';
 
 const app = express()
 const port = 3000
@@ -17,10 +18,13 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   const message = req.body.message
-
   const newText = debobardize(message)
-
   res.send(newText);
+
+  // Save to supabase.
+  const supabase = connectToSupabase(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+  addEntry(supabase, message)
+
 });
 
 app.listen(port, () => {
