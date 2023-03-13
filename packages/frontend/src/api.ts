085bde1a61afacaa1271ce1a0d0bf3f09e732cde
Flavien <saintsaens@users.noreply.github.com>
2023-03-13 20:22:23 +0100

    Update api.ts

--- packages/frontend/src/api.ts
+++ packages/frontend/src/api.ts
@@ -1,15 +1,16 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from "axios";
 
 const url = import.meta.env.VITE_BACKEND_URL;
 
 export async function postData(text: string) {
   try {
-    const response = await axios.post(url, {message: text});
+    const response: AxiosResponse = await axios.post(url, {message: text});
     const data = response.data;
 
     return data;
 
-  } catch (error) {
-    console.log(error);
+  } catch (error: any) {
+    console.error(`Error in postData function: ${error.message}`);
+    throw error;
   }
 }
