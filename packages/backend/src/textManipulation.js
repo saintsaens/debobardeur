b158828d4607feb78492cbff2a481733eb0b4b64
Flavien <saintsaens@users.noreply.github.com>
2023-04-20 18:35:34 +0200

    Update textManipulation.js

--- packages/backend/src/textManipulation.js
+++ packages/backend/src/textManipulation.js
@@ -13,8 +13,8 @@ export function replaceElementFromText(text, oldElement, newElement) {
 export function splitTextWithPunctuation(text) {
   // Split the string on spaces, but keep all punctuation including spaces; ignore hyphens, apostrophes and accentuated characters.
   const substrings = text.split(/(\s+|[^a-zA-ZÀ-ÿ-'’]+)/);
-
-  // Remove squash all non-character elements in the array (which can happen because of the "+"" in the regex).
+  
+  // Remove squash all non-character elements in the array (which can happen because of the "+" in the regex).
   for (let i = 0; i < substrings.length - 1; i++) {
     if (!substrings[i].match(/\w/) && !substrings[i + 1].match(/\w/)) {
       substrings[i] += substrings[i + 1];
