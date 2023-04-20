0a6df2b06df3da71bca915bb8150478798582109
Flavien <saintsaens@users.noreply.github.com>
2023-04-20 11:46:35 +0200

    Create 1 test case per file.

--- packages/backend/integrationTests/texts.test.ts
+++ packages/backend/integrationTests/texts.test.ts
@@ -1,24 +1,17 @@
-import { promises, readFileSync } from 'fs';
+import { promises, readFileSync, readdirSync } from 'fs';
 import { join } from 'path';
 import { debobardize } from '../src/debobardeur';
 import { test, expect } from "vitest";
 
-test('debobardizes txt files', async () => {
-  let txtFiles: string[] = [];
-  const files: string[] = await promises.readdir(__dirname);
-  for (let file of files) {
-    if (file.endsWith('.txt')) {
-      txtFiles.push(file);
-    }
-  }
+const txtFiles: string[] = readdirSync(__dirname)
+  .filter(file => file.endsWith('.txt'));
 
-  for (let file of txtFiles) {
-    const filePath: string = join(__dirname, "./" + file);
-    const data: string = readFileSync(filePath, 'utf8');
-    const test: string[] = data.split("-----").map((element: string) => element.trim());
-    const input: string = test[0];
-    const output: string = test[1];
+test.each(txtFiles)('debobardizes txt file: %s', (file) => {
+  const filePath: string = join(__dirname, file);
+  const data: string = readFileSync(filePath, 'utf8');
+  const test: string[] = data.split("-----").map((element: string) => element.trim());
+  const input: string = test[0];
+  const output: string = test[1];
 
-    expect(debobardize(input)).toBe(output);
-  }
+  expect(debobardize(input)).toBe(output);
 });
