7911dc73eac9bdc006101b9a4fec2361754c88f7
Flavien <saintsaens@users.noreply.github.com>
2023-05-11 12:32:08 +0200

    Update notion-export-rules.js
    feat: apply removal and replacement in a priority order.

--- packages/backend/src/notion/notion-export-rules.js
+++ packages/backend/src/notion/notion-export-rules.js
@@ -1,5 +1,5 @@
 import { removeRuleJsonFiles } from "../fileManipulation/file.js";
-import { getAllPagesInDatabase } from "./notion-db.js";
+import { getAllPagesInDatabaseSorted, getAllPagesInDatabase } from "./notion-db.js";
 import { getRulesRemplacer, getRulesSupprimer, writeRemplacerRulesIntoFile, writeSupprimerRulesIntoFile } from "./notion-rules.js";
 
 import { dirname, join } from 'path';
@@ -11,13 +11,13 @@ removeRuleJsonFiles()
 // Write replacing rules into file.
 const remplacerFilename = join(__dirname, "../../bobards/", process.env.REMPLACEMENT_FILENAME);
 
-const pagesRemplacer = await getAllPagesInDatabase(process.env.NOTION_DATABASE_ID_REMPLACER);
+const pagesRemplacer = await getAllPagesInDatabaseSorted(process.env.NOTION_DATABASE_ID_REMPLACER, process.env.NOTION_SORTING_PROPERTY_REMPLACER, process.env.NOTION_SORTING_PROPERTY_DIRECTION);
 const remplacerRulesObject = getRulesRemplacer(pagesRemplacer);
 writeRemplacerRulesIntoFile(remplacerRulesObject, remplacerFilename);
 
 // Write removing rules into file.
 const supprimerFilename = join(__dirname, "../../bobards/", process.env.BOBARDS_FILENAME);
 
-const pagesSupprimer = await getAllPagesInDatabase(process.env.NOTION_DATABASE_ID_BOBARDS);
+const pagesSupprimer = await getAllPagesInDatabaseSorted(process.env.NOTION_DATABASE_ID_BOBARDS, process.env.NOTION_SORTING_PROPERTY_SUPPRIMER, process.env.NOTION_SORTING_PROPERTY_DIRECTION);
 const supprimerRulesArray = getRulesSupprimer(pagesSupprimer);
 writeSupprimerRulesIntoFile(supprimerRulesArray, supprimerFilename);
