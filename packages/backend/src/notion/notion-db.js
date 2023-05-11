10a0e3b73b678ce1c53fbbb10269b2b16ab2acf0
Flavien <saintsaens@users.noreply.github.com>
2023-05-11 12:31:37 +0200

    Update notion-db.js
    feat: add function to get all pages in a database, sorted by priority.

--- packages/backend/src/notion/notion-db.js
+++ packages/backend/src/notion/notion-db.js
@@ -15,6 +15,30 @@ export async function getAllPagesInDatabase(notionDatabaseId) {
   }
 }
 
+export async function getAllPagesInDatabaseSorted(notionDatabaseId, sortingProperty, sortingDirection) {
+  try {
+    const response = await notion.databases.query({
+      database_id: notionDatabaseId,
+      sorts: [
+        {
+          // sortingProperty must be a string, the exact name of a database property.
+          property: sortingProperty,
+
+          // sortingDirection must be either "ascending" or "descending".
+          direction: sortingDirection,
+        },
+      ],
+    });
+
+    const pagesArray = getArrayOfPages(response);
+
+    return pagesArray;
+
+  } catch (error) {
+    console.error(error.body)
+  }
+}
+
 export function getArrayOfPages(pagesObject) {
   const pagesArray = pagesObject.results.map(page => page);
 
